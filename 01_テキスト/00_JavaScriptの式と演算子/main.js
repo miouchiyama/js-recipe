707092218a15130d4ea4ce789c21110be54c1a98
mio uchiyama <oitoito222@gmail.com>
2023-11-29 02:12:13 +0900

    [3の倍数]を学習しました。

--- 01_テキスト/00_JavaScriptの式と演算子/main.js
+++ 01_テキスト/00_JavaScriptの式と演算子/main.js
@@ -1,15 +1,19 @@
 // TRY: 練習問題１
+x = 57052 + 57054
+console.log(x)
+y = "114" + "106"
+console.log(y)
 
 // TRY: 練習問題２
-
+let x = 5;
+x += 2
+x *= 5
+x = 100 % x
+x /= 6
+console.log(x)
 // TRY: 練習問題３
 let happiness = 0 // 0 から 100 までの数値を代入してください
 let sleepiness = 0 // 0 から 100 までの数値を代入してください
-
-const isHappy = happiness > 90
-const isSleepy = sleepiness > 90
-
-const isNotHappyAndSleepy = !isHappy && isSleepy
 
 // 以下のコメントアウトを外して確認してみましょう
 // console.log("isNotHappyAndSleepy" + "は" + true + "ですか？", isNotHappyAndSleepy)
