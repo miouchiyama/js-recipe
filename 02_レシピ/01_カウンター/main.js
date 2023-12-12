39a708e0567a6a420d4e7484b4a85d71b1f17f34
mio uchiyama <oitoito222@gmail.com>
2023-12-12 20:34:16 +0900

    [ストップウォッチ]を学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,7 +1,9 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
-
+const display2 = document.getElementById("display2")
+const minusButton = document.getElementById("minus-button")
 let count = 0
+let count2 = 0
 
 // ボタン要素のonclickハンドラに関数を代入する
 plusButton.onclick = function() {
@@ -9,4 +11,11 @@ plusButton.onclick = function() {
   count += 1
   // count を表示
   display.textContent = count
+}
+
+minusButton.onclick = function() {
+    // count を更新
+    count2 -= 1
+    // count を表示
+    display2.textContent = count2
 }
