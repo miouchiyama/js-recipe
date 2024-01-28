110e93fc45b0ffd4ec5c5b612e345a046def4cf0
mio uchiyama <oitoito222@gmail.com>
2024-01-28 20:28:57 +0900

    ゼミweek3

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,17 +1,30 @@
 const figure = document.getElementById("figure")
 const squareButton = document.getElementById("square-button")
 const circleButton = document.getElementById("circle-button")
+const triangleButton = document.getElementById("triangle-button")
 
-figure.onclick = function() {
-    figure.classList.toggle("rounded")
+figure.onclick = function () {
+  figure.classList.toggle("rounded")
+  figure.classList.add("square")
+  figure.classList.remove("triangle")
 }
 
-circleButton.onclick = function() {
-    // figure に rounded クラスを追加する
-    figure.classList.add("rounded")
+circleButton.onclick = function () {
+  // figure に rounded クラスを追加する
+  figure.classList.add("rounded")
+  figure.classList.add("square")
+  figure.classList.remove("triangle")
 }
-  
-squareButton.onclick = function() {
-    // figure から rounded クラスを削除する
-    figure.classList.remove("rounded")
+
+squareButton.onclick = function () {
+  // figure から rounded クラスを削除する
+  figure.classList.remove("rounded")
+  figure.classList.add("square")
+  figure.classList.remove("triangle")
+}
+
+triangleButton.onclick = function () {
+  figure.classList.add("triangle")
+  figure.classList.remove("rounded")
+  figure.classList.remove("square")
 }
